0d15a7f906f9282b905168fb50c8a122e9b2325f
ThinkCat <624432528@qq.com>
2017-02-16 21:59:00 +0800

    update window style

--- src/main/java/com/jfx/Main.java
+++ src/main/java/com/jfx/Main.java
@@ -4,17 +4,26 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        BorderPane root = FXMLLoader.load(getClass().getResource("/root.fxml"));
+        primaryStage.setTitle("个人信息");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
+        //show person info
+        AnchorPane personView = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        root.setCenter(personView);
     }
 
     public static void main(String[] args) {
