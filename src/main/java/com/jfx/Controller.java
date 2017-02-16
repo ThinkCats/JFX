bfc19fe37d81a5ebc3b99c9465d677489a294d93
ThinkCat <624432528@qq.com>
2017-02-16 18:04:34 +0800

    Initial commit

---
+++ src/main/java/com/jfx/Controller.java
@@ -0,0 +1,13 @@
+package com.jfx;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void doCC(ActionEvent event) {
+        System.out.println("CCC");
+    }
+
+    public void doSS(ActionEvent event) {
+        System.out.println("SSS");
+    }
+}
